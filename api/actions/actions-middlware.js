f334b5cae61ce0b5c1abef48388cc67473d25f9e
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-04-08 10:15:18 -0600

    save

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,13 @@
 // add middlewares here related to actions
+const User = require('./actions-model')
+
+function logger(req, res, next){
+
+
+
+}
+
+
+module.exports = {
+    logger,
+}
--- api/server.js
+++ api/server.js
@@ -1,9 +1,45 @@
 const express = require('express');
+require('dotenv').config()
+const cors = require('cors')
+
 const server = express();
+
+server.use(express.json())
+server.use(cors())
+
+//const {logger} = require('./actions/actions-middlware')
+//server.use(logger)
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+
+
+
+
+server.get('/api/hello', (req, res)=> {
+    res.json({
+        message: 'api is working'
+    })
+})
+
+
+
+server.use('*',(req, res) => {
+    res.send(`<h1>Hello, there!</h1>`)
+})
+
+server.use((err, req,res, next)=>{ //eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+
+
+
 
 module.exports = server;
