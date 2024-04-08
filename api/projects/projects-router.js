1df252fab291256600cc0fe2f104b15f4a8cfbc7
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-04-08 10:45:20 -0600

    updated get response

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,49 @@
 // Write your "projects" router here!
+const express = require('express')
+const Post = require('./projects-model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Post.get()
+        .then (found => {
+         // Check if found array is empty
+         if (found.length === 0) {
+            // Respond with an empty array
+            res.json([]);
+        } else {
+            // Respond with the found projects
+            res.json(found);
+        }
+        }) 
+        .catch(err => {
+            res.status(500).json({
+                message:"The posts information could not be retrieved",
+                err:err.message,
+                stack: err.stack,
+            })
+        })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -10,6 +10,9 @@ server.use(cors())
 //const {logger} = require('./actions/actions-middlware')
 //server.use(logger)
 
+const projectsRouter = require('./projects/projects-router')
+server.use('api/projects', projectsRouter)
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
@@ -28,7 +31,9 @@ server.get('/api/hello', (req, res)=> {
 
 
 server.use('*',(req, res) => {
-    res.send(`<h1>Hello, there!</h1>`)
+    res.status(404).json({
+        message: "does not exist"
+    })
 })
 
 server.use((err, req,res, next)=>{ //eslint-disable-line
